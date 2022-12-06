a521f395ed20ab617c5ba4af6d836b343931161b
lsmnt <leasimonet56@gmail.com>
2022-12-06 16:42:50 +0100

    add property

---
+++ src/main/java/com/guillianv/magical/entity/spells/IUpgradable.java
@@ -0,0 +1,14 @@
+package com.guillianv.magical.entity.spells;
+
+public abstract class IUpgradable {
+
+
+    public int level = 1;
+
+    public  void Upgrade(){
+        this.level++;
+
+    }
+
+    public
+}
---
+++ src/main/java/com/guillianv/magical/entity/spells/UpgradeProperty.java
@@ -0,0 +1,37 @@
+package com.guillianv.magical.entity.spells;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.TranslatableContents;
+
+public class UpgradeProperty {
+
+    private TranslatableContents propertyName;
+    private ChatFormatting chatFormatting = ChatFormatting.WHITE;
+    public int level;
+    public double value;
+    public boolean isTime = false;
+
+    public UpgradeProperty(TranslatableContents translatableContents,double defaultValue){
+        this.propertyName = translatableContents;
+        this.level = 1;
+        this.value = defaultValue;
+
+    }
+
+    public UpgradeProperty isTime(){
+        this.isTime = true;
+        return this;
+    }
+
+    public UpgradeProperty PropertyColor(ChatFormatting chatFormatting){
+        this.chatFormatting = chatFormatting;
+        return this;
+    }
+
+
+    public Component getPropertyComponent(){
+        return Component.literal(this.propertyName.toString() +" "+ value);
+    }
+
+}
